7dbd558ca53198f30cd0616e35bcbe466aec60a3
Andrew Boudreau <andrewboudreau@gmail.com>
2021-06-19 01:55:34 -0500

    adding more methods

--- miner_httpclient.py
+++ miner_httpclient.py
@@ -22,14 +22,13 @@ class Client:
     
     def http_post(self, method, data=[""]):
         response = requests.post(self.url, json=self.jsonrpc_payload(method, data))
-        print(response.status_code)
-        return response.json()
+        return response.json()["result"]
 
-    # account
+    # accountq
 
     # blocks
     def block_height(self):
-        return self.http_post("block_height")["result"]
+        return self.http_post("block_height")
     
     def block(self, height=None):
         if height is None:
@@ -40,7 +39,50 @@ class Client:
     #def blockForHash(self, hash):
         # not sure what to do here
     
-
+    # dkg
+    def dkg_status(self):
+        return self.http_post("dkg_status")
         
+    def dkg_queue(self):
+        return self.http_post("dkg_queue")
+    
+    def dkg_next(self):
+        return self.http_post("dkg_next")
+    
+    def dkg_running(self):
+        return self.http_post("dkg_running")
+
+
+    # hbbft
+    def hbbft_status(self):
+        return self.http_post("hbbft_status")
+
+    def hbbft_queue(self):
+        return self.http_post("hbbft_queue")
+
+    def hbbft_skip(self):
+        return self.http_post("hbbft_skip")
+
+    # info
+    def info_height(self):
+        return self.http_post("info_height")
+    
+    def info_in_consensus(self):
+        return self.http_post("info_in_consensus")
+    
+    def info_name(self):
+        return self.http_post("info_name")
+    
+    def info_block_age(self):
+        return self.http_post("info_block_age")
+    
+    def info_p2p_status(self):
+        return self.http_post("info_p2p_status")
+    
+    def info_region(self):
+        return self.http_post("info_region")
+    
+    def info_summary(self):
+        return self.http_post("info_summary")
 
  # 1YSY5aooEh3LEt7sxDt1xdqw4cUd1gpwztQKE1fPsGRDozmJ2sw
